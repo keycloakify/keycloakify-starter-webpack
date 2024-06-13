e084f0c4aabe977963473a52fda7cb5ad017bfbb
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-13 21:51:17 +0200

    Update storybook to the latest version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [],
-    framework: {
-        name: "@storybook/react-vite",
-        options: {}
-    },
+    addons: [
+        "@storybook/preset-create-react-app",
+      ],
+      framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+      },
     staticDirs: ["../public"]
 };
 export default config;
