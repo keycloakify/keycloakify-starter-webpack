24786dbf150668ba55658bdb6260f40a45b3d387
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-07-27 18:10:09 +0200

    Update Keycloakify

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { StrictMode, lazy, Suspense } from "react";
-/*
+import { StrictMode } from "react";
+import { KcPage } from "./kc.gen";
+
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
+/*
 import { getKcContextMock } from "./login/KcPageStory";
 
-if (process.env.NODE_ENV === "development") {
+if (import.meta.env.DEV) {
     window.kcContext = getKcContextMock({
         pageId: "register.ftl",
         overrides: {}
@@ -13,29 +15,12 @@ if (process.env.NODE_ENV === "development") {
 }
 */
 
-const KcLoginThemePage = lazy(() => import("./login/KcPage"));
-//const KcAccountThemePage = lazy(() => import("./account/KcPage"));
-
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Suspense>
-            {(() => {
-                switch (window.kcContext?.themeType) {
-                    case "login":
-                        return <KcLoginThemePage kcContext={window.kcContext} />;
-                    //case "account":
-                    //    return <KcAccountThemePage kcContext={window.kcContext} />;
-                }
-                return <h1>No Keycloak Context</h1>;
-            })()}
-        </Suspense>
+        {!window.kcContext ? (
+            <h1>No Keycloak Context</h1>
+        ) : (
+            <KcPage kcContext={window.kcContext} />
+        )}
     </StrictMode>
 );
-
-declare global {
-    interface Window {
-        kcContext?:
-            | import("./login/KcContext").KcContext
-            //| import("./account/KcContext").KcContext;
-    }
-}
