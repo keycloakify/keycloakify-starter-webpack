4507831b8fc897acc27a3d129b702a754c8400f1
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-10 19:03:52 +0200

    Fix copy from vite

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
 
+/*
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
-/*
 import { getKcContextMock } from "./login/KcPageStory";
 
-if (import.meta.env.DEV) {
+if (process.env.NODE_ENV === "development") {
     window.kcContext = getKcContextMock({
         pageId: "register.ftl",
         overrides: {}
