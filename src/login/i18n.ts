a67d5c4cc1fe87e2112a1defddc7b99664d54bba
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-03 22:50:12 +0100

    Update keycloakify

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -1,7 +1,7 @@
 import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
-/** @see: https://docs.keycloakify.dev/i18n */
+/** @see: https://docs.keycloakify.dev/features/i18n */
 const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
 
 type I18n = typeof ofTypeI18n;
